be4adaa743af51836a45767a1617ab7343b08293
rachit2501 <rac.sri25@gmail.com>
2019-10-22 11:32:23 +0530

    context added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Navbar from './components/navbar';
 import SideBar from './components/sideBar';
 import Render from './components/render';
-
+import UserDetails from './components/context';
 
 function App() {
+  const username = "Rachit";
   return (
     <React.Fragment>
+      <UserDetails.Provider value = {username}>
       <Navbar />
       <SideBar />
       <Render />
+      </UserDetails.Provider>
     </React.Fragment>
   );
 }
---
+++ src/components/context.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const userDetails = createContext(["green" ,  ()=>{
+
+}]);
+
+export default userDetails;
