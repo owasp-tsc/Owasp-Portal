395363ca650ee50ef6a1a31be06c39878dc2ee6f
rachit2501 <rac.sri25@gmail.com>
2019-10-23 02:44:33 +0530

    leaderboard left

--- src/components/render.js
+++ src/components/render.js
@@ -1,10 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export class render extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       list : [{"Question" : ""}],
+       length: 0,
+       number : 0,
+       value : Number
+    }
+  }
+  
+  async componentDidMount()
+  {
+   var list = await axios.get('/questions');
+   //const list= [{"Question" : "asdsafas"  , "Answer" : 23}];
+
+   console.log(list);
+    this.setState({list , length:list.length});
+  }
+
+  onChange=(e)=>{
+    this.setState({[e.target.name] : e.target.value});
+  }
+  onClick=(e)=>{
+    console.log("lkafklsajfl");
+    this.setState({number: this.state.number+1});
+    console.log(this.state.number);   
+  }
+
     render() {
         return (
                 <main>
-                  {/* INSERT HERE */}
+                  {(this.state.length > this.state.number) && this.state.list[this.state.number].Question}
+                  <br></br>
+                  <input type="text/css" className="postionButton" value={this.state.value} name="value" onChange={this.onChange}></input>
+                  <button className="postionButton" onClick={this.onClick}>Submit</button>
                 </main>
               );
     }
