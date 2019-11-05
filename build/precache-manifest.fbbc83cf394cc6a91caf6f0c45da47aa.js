ed0ee4ec52a89dbf7268a10c054a3fc10866d50b
rachit2501 <rac.sri25@gmail.com>
2019-11-06 02:05:23 +0530

    biuld included

---
+++ build/precache-manifest.fbbc83cf394cc6a91caf6f0c45da47aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1538f5e0fb3a38d5d398adb07209fdfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84d8383bab2153554c0b",
+    "url": "/static/css/main.e8d032bf.chunk.css"
+  },
+  {
+    "revision": "f54751a7bc355420a83f",
+    "url": "/static/js/2.694793ed.chunk.js"
+  },
+  {
+    "revision": "84d8383bab2153554c0b",
+    "url": "/static/js/main.dcf111c2.chunk.js"
+  },
+  {
+    "revision": "0b03199d0cf8b4125f8f",
+    "url": "/static/js/runtime-main.a584c66c.js"
+  }
+]);
